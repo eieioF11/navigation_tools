545aebdf70b3b6d68d2e52bc22206c3ae485f254
f11 <eieioeiji0501@gmail.com>
2024-01-16 22:38:40 +0900

    [update]ノートパソコンで動作テスト

--- mpc_path_planning/launch/test.launch.py
+++ mpc_path_planning/launch/test.launch.py
@@ -6,10 +6,12 @@ import launch_ros.actions
 from ament_index_python.packages import get_package_share_directory
 from launch_ros.actions import ComposableNodeContainer
 from launch_ros.descriptions import ComposableNode
+from launch.substitutions import LaunchConfiguration, PythonExpression
 
 def generate_launch_description():
     share_dir = get_package_share_directory('mpc_path_planning')
-    rviz_config_file = os.path.join(share_dir, 'rviz','default.rviz')
+    rviz_config_file = os.path.join(share_dir, 'rviz','test.rviz')
+    map_file = os.path.join(share_dir, 'map','map.yaml')
     rviz2_node = launch_ros.actions.Node(
         package='rviz2',
         executable='rviz2',
@@ -45,8 +47,27 @@ def generate_launch_description():
         parameters=[os.path.join(share_dir, "config", "mpc_path_planning_param.yaml")],
         respawn=True,
     )
+    map_server_node= launch_ros.actions.Node(
+        package='nav2_map_server',
+        executable='map_server',
+        name='map_server',
+        namespace='',
+        output="screen",
+        parameters=[{
+            "yaml_filename": map_file
+        }],
+        respawn=True,
+    )
+    lifecycle_manager_node= launch_ros.actions.Node(
+        package='nav2_lifecycle_manager',
+        executable='lifecycle_manager',
+        name='lifecycle_manager_localization',
+        namespace='',
+        output="screen",
+        parameters=[{'autostart': True}, {'node_names': ['map_server']}],
+        respawn=True,
+    )
     return launch.LaunchDescription(
-        # [ map_to_base_link_node]
-        # [ mpc_path_planning_node, rviz2_node]
-        [ mpc_path_planning_node]
+        # [lifecycle_manager_node,map_server_node,mpc_path_planning_node,rviz2_node]
+        [ map_to_base_link_node,mpc_path_planning_node,rviz2_node, lifecycle_manager_node,map_server_node]
     )
