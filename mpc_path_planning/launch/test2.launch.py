2abf29ef142efdf410ea0d50a2fd0d1346916465
f11 <eieioeiji0501@gmail.com>
2024-01-23 10:13:43 +0900

    [update]point cloudからgrid map作成パッケージ完成

---
+++ mpc_path_planning/launch/test2.launch.py
@@ -0,0 +1,82 @@
+import os
+import sys
+from glob import glob
+from launch_ros.descriptions import ComposableNode
+from launch_ros.actions import Node
+from launch_ros.actions import ComposableNodeContainer
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+from launch.actions import DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+
+
+def generate_launch_description():
+    pkg_dir = get_package_share_directory('mpc_path_planning')
+    list = [
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                [os.path.join(pkg_dir, "launch"), "/rviz.launch.py"]
+            ),
+        ),
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                [os.path.join(pkg_dir, "launch"), "/mpc.launch.py"]
+            ),
+        ),
+        Node(
+            package="tf2_ros",
+            executable="static_transform_publisher",
+            arguments=[
+                "--x",
+                "-2.5",
+                "--y",
+                "0.0",
+                "--z",
+                "0.0",
+                "--yaw",
+                "0.0",
+                "--pitch",
+                "0.0",
+                "--roll",
+                "0.0",
+                "--frame-id",
+                "map",
+                "--child-frame-id",
+                "base_link",
+            ],
+        ),
+        Node(
+                package="tf2_ros",
+                executable="static_transform_publisher",
+                arguments=[
+                    "--x",
+                    "0.0",
+                    "--y",
+                    "0.0",
+                    "--z",
+                    "0.0",
+                    "--yaw",
+                    "0.0",
+                    "--pitch",
+                    "0.0",
+                    "--roll",
+                    "0.0",
+                    "--frame-id",
+                    "map",
+                    "--child-frame-id",
+                    "livox_frame",
+                ],
+        ),
+        Node(
+            package='pointcloud_to_grid',
+            executable='pointcloud_to_grid',
+            namespace='',
+            output="screen",
+            parameters=[os.path.join(get_package_share_directory('pointcloud_to_grid'), "config", "pointcloud_to_grid_param.yaml")],
+            respawn=True,
+        )
+    ]
+
+    return LaunchDescription(list)
--- pointcloud_to_grid/include/pointcloud_to_grid/pointcloud_to_grid_component.hpp
+++ pointcloud_to_grid/include/pointcloud_to_grid/pointcloud_to_grid_component.hpp
@@ -7,23 +7,14 @@
 #include <unordered_map>
 #include <utility>
 #include <vector>
-// Eigen
-#include <Eigen/Dense>
-#include <Eigen/Eigenvalues>
-// ROS
-#include <geometry_msgs/msg/pose_stamped.hpp>
-#include <geometry_msgs/msg/twist_stamped.hpp>
-#include <nav_msgs/msg/path.hpp>
 // PCL
+#include "common_lib/common_lib.hpp"
 #include "common_lib/pcl_utility/pcl_util.hpp"
-
 #include "common_lib/ros2_utility/msg_util.hpp"
-#include "common_lib/ros2_utility/tf_util.hpp"
 #include "common_lib/ros2_utility/ros_pcl_util.hpp"
 #include "extension_node/extension_node.hpp"
-
-#include "common_lib/common_lib.hpp"
-
+// OpenMP
+#include <omp.h>
 
 #define _ENABLE_ATOMIC_ALIGNMENT_FIX
 //******************************************************************************
@@ -44,73 +35,87 @@ public:
     const std::string & name_space = "",
     const rclcpp::NodeOptions & options = rclcpp::NodeOptions())
   : ExtensionNode("pointcloud_to_grid_node", name_space, options),
-    broadcaster_(this),
     tf_buffer_(this->get_clock()),
     listener_(tf_buffer_)
   {
     RCLCPP_INFO(this->get_logger(), "start pointcloud_to_grid_node");
     // get param
     std::string MAP_TOPIC = param<std::string>("pointcloud_to_grid.topic_name.map", "/map");
-    std::string CLOUD_TOPIC =
-      param<std::string>("pointcloud_to_grid.topic_name.cloud", "/cloud");
+    std::string CLOUD_TOPIC = param<std::string>("pointcloud_to_grid.topic_name.cloud", "/cloud");
     // frame
     MAP_FRAME = param<std::string>("pointcloud_to_grid.tf_frame.map_frame", "map");
     // setup
     VOXELGRID_SIZE = param<double>("pointcloud_to_grid.filter.voxelgrid_size", 0.04);
     Z_MAX = param<double>("pointcloud_to_grid.filter.z_max", 1.0);
     Z_MIN = param<double>("pointcloud_to_grid.filter.z_min", 0.0);
+    gmap.info.resolution = param<double>("pointcloud_to_grid.grid_map_info.resolution", 1.0);
+    double width = param<double>("pointcloud_to_grid.grid_map_info.width", 50.0);
+    double height = param<double>("pointcloud_to_grid.grid_map_info.height", 50.0);
+    gmap.info.width = std::round(width / gmap.info.resolution);
+    gmap.info.height = std::round(height / gmap.info.resolution);
+    gmap.info.origin.position.x = -0.5 * width;
+    gmap.info.origin.position.y = -0.5 * height;
+    gmap.resize(gmap.info.width, gmap.info.height);
     // init
     initialization_ = true;
     // publisher
     map_pub_ = this->create_publisher<nav_msgs::msg::OccupancyGrid>(MAP_TOPIC, rclcpp::QoS(10));
-    debug_cloud_pub_ = this->create_publisher<sensor_msgs::msg::PointCloud2>("pointcloud_to_grid/debug_points", rclcpp::QoS(10));
+    debug_cloud_pub_ = this->create_publisher<sensor_msgs::msg::PointCloud2>(
+      "pointcloud_to_grid/debug_points", rclcpp::QoS(10));
     // subscriber
-    cloud_sub_ = this->create_subscription<sensor_msgs::msg::PointCloud2>(CLOUD_TOPIC, rclcpp::QoS(10),
-    [&](const sensor_msgs::msg::PointCloud2::SharedPtr msg)
-    {
-      sensor_msgs::msg::PointCloud2 get_cloud;
-      get_cloud = *msg;
-      std::optional<sensor_msgs::msg::PointCloud2> trans_cloud = transform_pointcloud2(tf_buffer_, MAP_FRAME, get_cloud);
-      if (!trans_cloud)
-      {
-        RCLCPP_ERROR(this->get_logger(), "transform error");
-        return;
-      }
-      // msg convert
-      pcl::PointCloud<pcl::PointXYZ> cloud;
-      pcl::fromROSMsg(trans_cloud.value(), cloud);
-      if (cloud.empty())
-      {
-        RCLCPP_WARN(this->get_logger(), "cloud empty");
-        return;
-      }
-      // nan値除去
-      std::vector<int> mapping;
-      pcl::removeNaNFromPointCloud(cloud, cloud, mapping);
-      cloud = voxelgrid_filter(cloud, VOXELGRID_SIZE, VOXELGRID_SIZE, VOXELGRID_SIZE);
-      pcl::PointCloud<pcl::PointXYZ> cut_cloud = passthrough_filter("z",cloud, Z_MIN, Z_MAX);
-      debug_cloud_pub_->publish(make_ros_pointcloud2(make_header(MAP_FRAME, get_cloud.header.stamp), cut_cloud));
-    });
+    cloud_sub_ = this->create_subscription<sensor_msgs::msg::PointCloud2>(
+      CLOUD_TOPIC, rclcpp::QoS(10), [&](const sensor_msgs::msg::PointCloud2::SharedPtr msg) {
+        rclcpp::Time start_time = rclcpp::Clock().now();
+        sensor_msgs::msg::PointCloud2 get_cloud;
+        get_cloud = *msg;
+        std::optional<sensor_msgs::msg::PointCloud2> trans_cloud =
+          transform_pointcloud2(tf_buffer_, MAP_FRAME, get_cloud);
+        if (!trans_cloud) {
+          RCLCPP_ERROR(this->get_logger(), "transform error");
+          return;
+        }
+        // msg convert
+        pcl::PointCloud<pcl::PointXYZ> cloud;
+        pcl::fromROSMsg(trans_cloud.value(), cloud);
+        if (cloud.empty()) {
+          RCLCPP_WARN(this->get_logger(), "cloud empty");
+          return;
+        }
+        // nan値除去
+        std::vector<int> mapping;
+        pcl::removeNaNFromPointCloud(cloud, cloud, mapping);
+        cloud = voxelgrid_filter(cloud, VOXELGRID_SIZE, VOXELGRID_SIZE, VOXELGRID_SIZE);
+        pcl::PointCloud<pcl::PointXYZ> cut_cloud = passthrough_filter("z", cloud, Z_MIN, Z_MAX);
+        debug_cloud_pub_->publish(
+          make_ros_pointcloud2(make_header(MAP_FRAME, get_cloud.header.stamp), cut_cloud));
+#pragma omp parallel for
+        for (const auto point : cut_cloud.points) {
+          Vector2d p = gmap.get_grid_pos(
+            conversion_vector2<pcl::PointXYZ, Vector2d>(point));  //グリッド上の位置取得
+          if (gmap.is_contain(p))  //その点がgmap.info.widthとgmap.info.heightの範囲内かどうか
+            gmap.set(p, GridMap::WALL_VALUE);
+        }
+        map_pub_->publish(make_nav_gridmap(make_header(MAP_FRAME, get_cloud.header.stamp), gmap));
+        double proc_time = (rclcpp::Clock().now() - start_time).seconds() * 1000.0;
+        RCLCPP_INFO(this->get_logger(), "proc_time:%f[ms]", proc_time);
+      });
   }
 
 private:
   bool initialization_;
-  double  VOXELGRID_SIZE;
-  double  Z_MAX;
-  double  Z_MIN;
+  double VOXELGRID_SIZE;
+  double Z_MAX;
+  double Z_MIN;
   // param
   std::string MAP_FRAME;
   // tf
-  tf2_ros::TransformBroadcaster broadcaster_;
   tf2_ros::Buffer tf_buffer_;
   tf2_ros::TransformListener listener_;
-  // timer
-  rclcpp::TimerBase::SharedPtr main_timer_;
-	// subscriber
-	rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr cloud_sub_;
-	// publisher
-	rclcpp::Publisher<nav_msgs::msg::OccupancyGrid>::SharedPtr map_pub_;
+  // subscriber
+  rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr cloud_sub_;
+  // publisher
+  rclcpp::Publisher<nav_msgs::msg::OccupancyGrid>::SharedPtr map_pub_;
   rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr debug_cloud_pub_;
   // map
-  nav_msgs::msg::OccupancyGrid map_msg_;
+  GridMap gmap;
 };
