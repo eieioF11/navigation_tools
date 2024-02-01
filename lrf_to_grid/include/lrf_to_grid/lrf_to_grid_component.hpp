8f0deea15e1478966c1f2183e1b076064ada54ad
f11 <eieioeiji0501@gmail.com>
2024-02-01 18:06:07 +0900

    [update]屋外テストの準備

--- lrf_to_grid/include/lrf_to_grid/lrf_to_grid_component.hpp
+++ lrf_to_grid/include/lrf_to_grid/lrf_to_grid_component.hpp
@@ -1,19 +1,19 @@
 #pragma once
 #include <execution>
 #include <iostream>
+#include <laser_geometry/laser_geometry.hpp>
 #include <mutex>
 #include <optional>
 #include <string>
 #include <unordered_map>
 #include <utility>
 #include <vector>
-
-#include <laser_geometry/laser_geometry.hpp>
 // PCL
 #include "common_lib/common_lib.hpp"
 #include "common_lib/pcl_utility/pcl_util.hpp"
 #include "common_lib/ros2_utility/msg_util.hpp"
 #include "common_lib/ros2_utility/ros_pcl_util.hpp"
+#include "common_lib/ros2_utility/tf_util.hpp"
 #include "extension_node/extension_node.hpp"
 // OpenMP
 #include <omp.h>
@@ -23,7 +23,7 @@
 // for文の実行方法設定
 //  #define LOOP_MODE std::execution::seq // 逐次実行
 //  #define LOOP_MODE std::execution::par // 並列実行
-#define LOOP_MODE std::execution::par_unseq // 並列化・ベクトル化
+#define LOOP_MODE std::execution::par_unseq  // 並列化・ベクトル化
 // デバック関連設定
 #define DEBUG_OUTPUT
 //******************************************************************************
@@ -32,13 +32,13 @@ using namespace std::chrono_literals;
 class LRFToGrid : public ExtensionNode
 {
 public:
-  LRFToGrid(const rclcpp::NodeOptions &options) : LRFToGrid("", options) {}
+  LRFToGrid(const rclcpp::NodeOptions & options) : LRFToGrid("", options) {}
   LRFToGrid(
-      const std::string &name_space = "",
-      const rclcpp::NodeOptions &options = rclcpp::NodeOptions())
-      : ExtensionNode("lrf_to_grid_node", name_space, options),
-        tf_buffer_(this->get_clock()),
-        listener_(tf_buffer_)
+    const std::string & name_space = "",
+    const rclcpp::NodeOptions & options = rclcpp::NodeOptions())
+  : ExtensionNode("lrf_to_grid_node", name_space, options),
+    tf_buffer_(this->get_clock()),
+    listener_(tf_buffer_)
   {
     RCLCPP_INFO(this->get_logger(), "start lrf_to_grid_node");
     // get param
@@ -47,36 +47,37 @@ public:
     std::string CLOUD_TOPIC = param<std::string>("lrf_to_grid.topic_name.cloud", "/cloud");
     // frame
     MAP_FRAME = param<std::string>("lrf_to_grid.tf_frame.map_frame", "map");
+    ROBOT_FRAME = param<std::string>("lrf_to_grid.tf_frame.robot_frame", "base_link");
     // setup
     ADD_CELL = param<bool>("lrf_to_grid.add_cell", false);
     VOXELGRID_SIZE = param<double>("lrf_to_grid.filter.voxelgrid_size", 0.04);
     Z_MAX = param<double>("lrf_to_grid.filter.z_max", 1.0);
     Z_MIN = param<double>("lrf_to_grid.filter.z_min", 0.0);
     gmap_.info.resolution = param<double>("lrf_to_grid.grid_map_info.resolution", 1.0);
-    double width = param<double>("lrf_to_grid.grid_map_info.width", 50.0);
-    double height = param<double>("lrf_to_grid.grid_map_info.height", 50.0);
-    gmap_.info.width = std::round(width / gmap_.info.resolution);
-    gmap_.info.height = std::round(height / gmap_.info.resolution);
-    gmap_.info.origin.position.x = -0.5 * width;
-    gmap_.info.origin.position.y = -0.5 * height;
+    WIDTH = param<double>("lrf_to_grid.grid_map_info.width", 50.0);
+    HEIGHT = param<double>("lrf_to_grid.grid_map_info.height", 50.0);
+    gmap_.info.width = std::round(WIDTH / gmap_.info.resolution);
+    gmap_.info.height = std::round(HEIGHT / gmap_.info.resolution);
+    gmap_.info.origin.position.x = -0.5 * WIDTH;
+    gmap_.info.origin.position.y = -0.5 * HEIGHT;
     gmap_.resize(gmap_.info.width, gmap_.info.height);
     // init
     // publisher
     map_pub_ =
-        this->create_publisher<nav_msgs::msg::OccupancyGrid>(MAP_TOPIC, rclcpp::QoS(10).reliable());
+      this->create_publisher<nav_msgs::msg::OccupancyGrid>(MAP_TOPIC, rclcpp::QoS(10).reliable());
     debug_cloud_pub_ = this->create_publisher<sensor_msgs::msg::PointCloud2>(
-        "lrf_to_grid/debug_points", rclcpp::QoS(10));
+      "lrf_to_grid/debug_points", rclcpp::QoS(10));
     // subscriber
     laser_sub_ = this->create_subscription<sensor_msgs::msg::LaserScan>(
-        LASER_TOPIC, rclcpp::QoS(10), [&](const sensor_msgs::msg::LaserScan::SharedPtr msg)
-        {
-          sensor_msgs::msg::PointCloud2 get_cloud;
-          projector_.projectLaser(*msg, get_cloud);
-          get_cloud.header = msg->header;
-          make_grid_map(get_cloud); });
+      LASER_TOPIC, rclcpp::QoS(10), [&](const sensor_msgs::msg::LaserScan::SharedPtr msg) {
+        sensor_msgs::msg::PointCloud2 get_cloud;
+        projector_.projectLaser(*msg, get_cloud);
+        get_cloud.header = msg->header;
+        make_grid_map(get_cloud);
+      });
     cloud_sub_ = this->create_subscription<sensor_msgs::msg::PointCloud2>(
-        CLOUD_TOPIC, rclcpp::QoS(10), [&](const sensor_msgs::msg::PointCloud2::SharedPtr msg)
-        { make_grid_map(*msg); });
+      CLOUD_TOPIC, rclcpp::QoS(10),
+      [&](const sensor_msgs::msg::PointCloud2::SharedPtr msg) { make_grid_map(*msg); });
   }
 
 private:
@@ -84,9 +85,13 @@ private:
   double VOXELGRID_SIZE;
   double Z_MAX;
   double Z_MIN;
-  const std::array<Vector2d, 8> nb = {Vector2d{1, 0}, {0, 1}, {-1, 0}, {0, -1}, {1, 1}, {-1, 1}, {-1, -1}, {1, -1}};
+  double WIDTH;
+  double HEIGHT;
+  const std::array<Vector2d, 8> nb = {Vector2d{1, 0}, {0, 1},  {-1, 0},  {0, -1},
+                                      {1, 1},         {-1, 1}, {-1, -1}, {1, -1}};
   // param
   std::string MAP_FRAME;
+  std::string ROBOT_FRAME;
   // tf
   tf2_ros::Buffer tf_buffer_;
   tf2_ros::TransformListener listener_;
@@ -101,68 +106,75 @@ private:
   // map
   GridMap gmap_;
 
-  void make_grid_map(const sensor_msgs::msg::PointCloud2 &get_cloud)
+  void make_grid_map(const sensor_msgs::msg::PointCloud2 & get_cloud)
   {
     if (!tf_buffer_.canTransform(
-            get_cloud.header.frame_id, MAP_FRAME, get_cloud.header.stamp,
-            tf2::durationFromSec(1.0)))
-    { // 変換無いよ
+          get_cloud.header.frame_id, MAP_FRAME, get_cloud.header.stamp,
+          tf2::durationFromSec(1.0))) {  // 変換無いよ
       RCLCPP_WARN(
-          this->get_logger(), "%s %s can not Transform", MAP_FRAME.c_str(), get_cloud.header.frame_id.c_str());
+        this->get_logger(), "%s %s can not Transform", MAP_FRAME.c_str(),
+        get_cloud.header.frame_id.c_str());
       return;
     }
-    rclcpp::Time start_time = rclcpp::Clock().now();
-    std::optional<sensor_msgs::msg::PointCloud2> trans_cloud =
+    if (!tf_buffer_.canTransform(
+          ROBOT_FRAME, MAP_FRAME, rclcpp::Time(0),
+          tf2::durationFromSec(1.0))) {  // 変換無いよ
+      RCLCPP_WARN(
+        this->get_logger(), "%s %s can not Transform", MAP_FRAME.c_str(), ROBOT_FRAME.c_str());
+      return;
+    }
+    auto map_to_base_link = lookup_transform(tf_buffer_, ROBOT_FRAME, MAP_FRAME);
+    if (map_to_base_link) {
+      rclcpp::Time start_time = rclcpp::Clock().now();
+      Pose3d base_link_pose = make_pose(map_to_base_link.value().transform);
+      gmap_.info.origin.position.x = base_link_pose.position.x-0.5 * WIDTH;
+      gmap_.info.origin.position.y = base_link_pose.position.y-0.5 * HEIGHT;
+      std::optional<sensor_msgs::msg::PointCloud2> trans_cloud =
         transform_pointcloud2(tf_buffer_, MAP_FRAME, get_cloud);
-    if (!trans_cloud)
-    {
-      RCLCPP_ERROR(this->get_logger(), "transform error");
-      return;
-    }
-    // msg convert
-    pcl::PointCloud<pcl::PointXYZ> cloud;
-    pcl::fromROSMsg(trans_cloud.value(), cloud);
-    if (cloud.empty())
-    {
-      RCLCPP_WARN(this->get_logger(), "cloud empty");
-      return;
-    }
-    // nan値除去
-    std::vector<int> mapping;
-    pcl::removeNaNFromPointCloud(cloud, cloud, mapping);
-    cloud = voxelgrid_filter(cloud, VOXELGRID_SIZE, VOXELGRID_SIZE, VOXELGRID_SIZE);
-    pcl::PointCloud<pcl::PointXYZ> cut_cloud = passthrough_filter("z", cloud, Z_MIN, Z_MAX);
-    debug_cloud_pub_->publish(
+      if (!trans_cloud) {
+        RCLCPP_ERROR(this->get_logger(), "transform error");
+        return;
+      }
+      // msg convert
+      pcl::PointCloud<pcl::PointXYZ> cloud;
+      pcl::fromROSMsg(trans_cloud.value(), cloud);
+      if (cloud.empty()) {
+        RCLCPP_WARN(this->get_logger(), "cloud empty");
+        return;
+      }
+      // nan値除去
+      std::vector<int> mapping;
+      pcl::removeNaNFromPointCloud(cloud, cloud, mapping);
+      cloud = voxelgrid_filter(cloud, VOXELGRID_SIZE, VOXELGRID_SIZE, VOXELGRID_SIZE);
+      pcl::PointCloud<pcl::PointXYZ> cut_cloud = passthrough_filter("z", cloud, Z_MIN, Z_MAX);
+      debug_cloud_pub_->publish(
         make_ros_pointcloud2(make_header(MAP_FRAME, get_cloud.header.stamp), cut_cloud));
-    // 初期化
+      // 初期化
 #pragma omp parallel for
-    for (auto &cell : gmap_.data)
-      cell = 0;
-      // マップ作成
+      for (auto & cell : gmap_.data) cell = 0;
+        // マップ作成
 #pragma omp parallel for
-    for (const auto point : cut_cloud.points)
-    {
-      Vector2d p = gmap_.get_grid_pos(
-          conversion_vector2<pcl::PointXYZ, Vector2d>(point)); // グリッド上の位置取得
-      if (gmap_.is_contain(p))                                 // その点がgmap_.info.widthとgmap_.info.heightの範囲内かどうか
-        if (!gmap_.is_wall(gmap_.at(p)))
-          gmap_.set(p, GridMap::WALL_VALUE);
-      if (ADD_CELL)
-      {
-        for (size_t i = 0; i < 8; i++)
-        {
-          Vector2d np = p + nb[i];
-          if (gmap_.is_contain(np)) // その点がgmap_.info.widthとgmap_.info.heightの範囲内かどうか
-            if (!gmap_.is_wall(gmap_.at(np)))
-            {
+      for (const auto point : cut_cloud.points) {
+        Vector2d p = gmap_.get_grid_pos(
+          conversion_vector2<pcl::PointXYZ, Vector2d>(point));  // グリッド上の位置取得
+        if (gmap_.is_contain(p))  // その点がgmap_.info.widthとgmap_.info.heightの範囲内かどうか
+          if (!gmap_.is_wall(gmap_.at(p))) gmap_.set(p, GridMap::WALL_VALUE);
+        if (ADD_CELL) {
+          for (size_t i = 0; i < 8; i++) {
+            Vector2d np = p + nb[i];
+            if (gmap_.is_contain(
+                  np))  // その点がgmap_.info.widthとgmap_.info.heightの範囲内かどうか
+              if (!gmap_.is_wall(gmap_.at(np))) {
 #pragma omp critical
-              gmap_.set(np, GridMap::WALL_VALUE);
-            }
+                gmap_.set(np, GridMap::WALL_VALUE);
+              }
+          }
         }
       }
+      map_pub_->publish(make_nav_gridmap(make_header(MAP_FRAME, get_cloud.header.stamp), gmap_));
+      double proc_time =
+        unit_cast<unit::time::s, unit::time::ms>((rclcpp::Clock().now() - start_time).seconds());
+      RCLCPP_INFO(this->get_logger(), "proc_time:%f[ms]", proc_time);
     }
-    map_pub_->publish(make_nav_gridmap(make_header(MAP_FRAME, get_cloud.header.stamp), gmap_));
-    double proc_time = unit_cast<unit::time::s, unit::time::ms>((rclcpp::Clock().now() - start_time).seconds());
-    RCLCPP_INFO(this->get_logger(), "proc_time:%f[ms]", proc_time);
   }
 };
---
+++ mpc_path_planning/launch/go1_outside_test.launch.py
@@ -0,0 +1,64 @@
+import os
+import sys
+from glob import glob
+from launch_ros.descriptions import ComposableNode
+from launch_ros.actions import Node
+from launch_ros.actions import ComposableNodeContainer
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+from launch.actions import DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+
+
+def generate_launch_description():
+    pkg_dir = get_package_share_directory('mpc_path_planning')
+    list = [
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                [os.path.join(pkg_dir, "launch"), "/rviz.launch.py"]
+            ),
+        ),
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                [os.path.join(pkg_dir, "launch"), "/mpc.launch.py"]
+            ),
+            launch_arguments={
+                'config': os.path.join(pkg_dir, "config", "robot_outside_mpc_path_planning_param.yaml"),
+            }.items()
+        ),
+        Node(
+            package="tf2_ros",
+            executable="static_transform_publisher",
+            arguments=[
+                "--x",
+                "-0.26",
+                "--y",
+                "0.0",
+                "--z",
+                "0.0",
+                "--yaw",
+                "0.0",
+                "--pitch",
+                "0.0",
+                "--roll",
+                "0.0",
+                "--frame-id",
+                "imu",
+                "--child-frame-id",
+                "base_link",
+            ],
+        ),
+        Node(
+            package='lrf_to_grid',
+            executable='lrf_to_grid',
+            namespace='',
+            # output="screen",
+            arguments=['--ros-args', '--log-level', 'WARN'],
+            parameters=[os.path.join(get_package_share_directory('lrf_to_grid'), "config", "lrf_to_grid_outside_param.yaml")],
+            respawn=True,
+        )
+    ]
+
+    return LaunchDescription(list)
